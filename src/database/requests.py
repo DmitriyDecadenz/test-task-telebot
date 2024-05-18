bddb51f4f94c9ee5dbcae7212b5151dae70c23d5
DmitriyDecadenz <dima.hubul@icloud.com>
2024-05-19 00:52:59 +0400

    add typing and doc string

--- src/database/requests.py
+++ src/database/requests.py
@@ -1,10 +1,11 @@
-from typing import Any
 from src.database.models import async_session
 from src.database.models import UserModel, TaskModel
-from sqlalchemy import select, insert
+from sqlalchemy import select
+from typing import List
 
 
 async def set_user(tg_id: int) -> None:
+    """Add user in db"""
     async with async_session() as session:
         user = await session.scalar(select(UserModel).where(UserModel.tg_id == tg_id))
 
@@ -13,15 +14,16 @@ async def set_user(tg_id: int) -> None:
             await session.commit()
 
 
-async def get_tasks():
+async def get_tasks() -> List[TaskModel]:
+    """Return tasks list from db"""
     async with async_session() as session:
         tasks_query = select(TaskModel)
         result = await session.execute(tasks_query)
         return result.scalars().all()
-        # return await session.scalars(select(TaskModel))
 
 
 async def add_task(description: str) -> None:
+    """Add task in db"""
     async with async_session() as session:
         task = await session.scalar(select(TaskModel).where(TaskModel.description == description))
 
